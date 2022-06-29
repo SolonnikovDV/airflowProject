c060e54b2afb031096ceff7a85fc0e8a31f11967
Solonnikov.dv@gmail.com <46959388+SolonnikovDV@users.noreply.github.com>
2022-06-29 17:21:20 +0300

    add loop for loading/insert data from API

--- table.py
+++ table.py
@@ -1,6 +1,11 @@
 import datetime
+import os
+import time
+
 import psycopg2
 import requests
+import schedule as schedule
+
 import config as cfg
 
 DATA_TIME = datetime.datetime.now()
@@ -13,12 +18,11 @@ def get_data_from_api():
     print(f'status API connect : {response.status_code}')
     if response.status_code != 200:
         data = response.text
-        print(f'data from API :\n{data}')
+        # print(f'data from API :\n{data}')
         pass
     else:
         data = response.json()['data']  # dict type
-
-        print(f'data from API :\n{data}')
+        # print(f'data from API :\n{data}')
         return data
 
 
@@ -40,25 +44,7 @@ def create_connection_to_psql(host, port, sslmode, dbname, user, password):
         return conn
 
 
-def create_table_query():
-    return f"""CREATE TABLE IF NOT EXISTS {cfg.DB_TABLE_NAME}(
-                id VARCHAR ,
-                symbol VARCHAR,
-                currencySymbol VARCHAR ,
-                type VARCHAR ,
-                rateUsd VARCHAR);"""
-
-
-def insert_query():
-    return f"""INSERT INTO {cfg.DB_TABLE_NAME}
-    VALUES (%(id)s, %(symbol)s, %(currencySymbol)s, %(type)s, %(rateUsd)s);"""
-
-
-# func include connection to DB
-def handle_table():
-    data = (get_data_from_api())
-
-    # try connection to DB
+def create_table():
     try:
         conn = create_connection_to_psql(
             cfg.HOST,
@@ -69,16 +55,18 @@ def handle_table():
             cfg.PASSWORD
         )
         conn.autocommit = True
-
-        # create table
+    # create table
         with conn.cursor() as cur:
-            cur.execute(create_table_query())
+            sql_query = f"""
+            CREATE TABLE IF NOT EXISTS {cfg.DB_TABLE_NAME}(
+            id VARCHAR, 
+            symbol VARCHAR, 
+            currencySymbol VARCHAR, 
+            type VARCHAR, 
+            rateUsd VARCHAR
+            );"""
+            cur.execute(sql_query)
             print(f'[INFO] : {DATA_TIME} Table created successful')
-
-        # insert data into table
-        with conn.cursor() as cur:
-            cur.execute(insert_query(), data)
-            print(f'[INFO] : {DATA_TIME} Data <{data}> inserted in the table <{cfg.DB_TABLE_NAME}>')
 
     except Exception as e:
         print(f'[INFO] : {DATA_TIME} Error connection : ', e)
@@ -88,5 +76,46 @@ def handle_table():
             print(f'[INFO] : {DATA_TIME} Psql connection closed')
 
 
+def insert_data_to_table():
+    try:
+        conn = create_connection_to_psql(
+            cfg.HOST,
+            cfg.PORT,
+            cfg.SSLMODE,
+            cfg.DBNAME,
+            cfg.USER,
+            cfg.PASSWORD
+        )
+        conn.autocommit = True
+        with conn.cursor() as cur:
+            sql_query = f"""
+            INSERT INTO {cfg.DB_TABLE_NAME} 
+            VALUES(
+            %(id)s, 
+            %(symbol)s, 
+            %(currencySymbol)s, 
+            %(type)s, 
+            %(rateUsd)s
+            );"""
+            data = (get_data_from_api())
+            cur.execute(sql_query, data)
+            print(f'''[INFO] : {DATA_TIME} Data RATE_USD <{data['rateUsd']}> inserted in the table <{cfg.DB_TABLE_NAME}>''')
+
+    except Exception as e:
+        print(f'[INFO] : {DATA_TIME} Error connection : ', e)
+    finally:
+        if conn:
+            conn.close()
+            print(f'[INFO] : {DATA_TIME} Psql connection closed')
+
+
+count = 0
+
 if __name__ == '__main__':
-    handle_table()
+    print('')
+    # create_table()
+    while True:
+        count += 1
+        print(f'Loading data.......... \nNumber of loading {count}')
+        insert_data_to_table()
+        time.sleep(30)
